4ee37edcd94ef87a782a45823b6795af2ba936fe
AmaliaRVM <amaliarvm@gmail.com>
2022-03-16 16:59:41 +0100

    fetch

--- quotes.js
+++ quotes.js
@@ -2,29 +2,50 @@ const quoteTag = document.querySelector('h1');
 const authorTag = document.querySelector('p');
 const randomButton = document.querySelector('img');
 
+
 const url = "https://api.superhi.com/api/test/quotes/";
 
 let data = [];
 
+const colors = ['#e775eb', '#7581eb', '#ce3434', '#75ebb4'];
+
 //fetch
-/* fetch("quotes.json").then(function(response){
-    return response.json
-}).then(function(dataJson){
-    console.log(dataJson)
-}) */
+catchData();
+
+async function catchData (){
+    const response = await fetch(url);
+    const json = await response.json();
+    data = json
+    getQuote() 
+}
+
+/* fetch(url)
+.then(response => response.json())
+.then((jsonData) => {
+    data = jsonData
+    getQuote()
+} 
+) */
 
 //get quote function
 const getQuote = function (){
 
     if (data.length > 0){
         
-        //get a random quoteto the random button
+        //get a random quote the random button
         const randomNumber = Math.floor(Math.random() * data.length);
         const randomQuote = data[randomNumber];
+
+        //get random colors
+        const randomColorNumber = Math.floor(Math.random() * colors.length);
+        const randomColor =  colors[randomColorNumber];
     
         //put it in the site
         quoteTag.innerHTML = randomQuote.quote;
         authorTag.innerHTML = randomQuote.author;
+
+        //put it in the background
+        authorTag.style.color = randomColor;
 
     }
 
